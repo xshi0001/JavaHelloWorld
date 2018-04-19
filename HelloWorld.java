2ebbfda8758bdf4b0c0f2a289923d9678818bbaa
xshi0001 <502446975@qq.com>
2018-04-19 15:13:10 +0800

    添加了hello

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package JavaHelloWorld;
+
+/**
+ * Created by admin on 2018/4/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+    }
+}
